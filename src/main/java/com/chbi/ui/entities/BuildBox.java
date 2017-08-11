6623666527ede4dd6e8a991425577b5de1869de2
Christoph Birk <christoph.birk@googlemail.com>
2017-08-11 20:47:45 +0200

    Remove foxed todo.

--- src/main/java/com/chbi/ui/entities/BuildBox.java
+++ src/main/java/com/chbi/ui/entities/BuildBox.java
@@ -1,10 +1,7 @@
 package com.chbi.ui.entities;
 
-import com.chbi.json.entities.JenkinsJob;
-
 /**
  * Class is representing a buildbox on the view.
- * //TODO: implement filling and using this class instead of {@link JenkinsJob}.
  */
 public class BuildBox {
 
