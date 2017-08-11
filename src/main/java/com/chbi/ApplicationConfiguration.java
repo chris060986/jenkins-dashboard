7a1b48c8685d6b2af5bc02630e54e541dcec57d1
Christoph Birk <christoph.birk@googlemail.com>
2017-08-11 14:33:48 +0200

    Moving more elements to config file

--- src/main/java/com/chbi/ApplicationConfiguration.java
+++ src/main/java/com/chbi/ApplicationConfiguration.java
@@ -1,5 +1,6 @@
 package com.chbi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,32 @@ public class ApplicationConfiguration {
 
     String baseUrl;
 
+    @Value("${url.searchPattern}")
+    String urlSearchPattern;
+    @Value("${url.replacement}")
+    String urlReplacement;
+
     public String getBaseUrl() {
         return baseUrl;
   }
 
     public void setBaseUrl(String baseUrl) {
         this.baseUrl = baseUrl;
+  }
+
+  public String getUrlSearchPattern() {
+    return urlSearchPattern;
+  }
+
+  public void setUrlSearchPattern(String urlSearchPattern) {
+    this.urlSearchPattern = urlSearchPattern;
+  }
+
+  public String getUrlReplacement() {
+    return urlReplacement;
+  }
+
+  public void setUrlReplacement(String urlReplacement) {
+    this.urlReplacement = urlReplacement;
   }
 }
--- src/main/java/com/chbi/rest/DataProvider.java
+++ src/main/java/com/chbi/rest/DataProvider.java
@@ -5,8 +5,14 @@ import com.chbi.json.entities.JenkinsBuild;
 import com.chbi.json.entities.JenkinsBuildPipeline;
 import com.chbi.json.entities.JenkinsJob;
 import com.chbi.json.entities.JenkinsView;
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -23,19 +29,41 @@ public class DataProvider {
     public List<JenkinsJob> getJenkinsJobs(){
         RestTemplate restTemplate = new RestTemplate();
 
-        String preparedUrl = prepareUrl(configuration.getBaseUrl());
-        System.out.println(preparedUrl);
+        HttpEntity<String> request = getStringHttpEntity();
 
-        JenkinsView jenkinsView = restTemplate.getForObject(preparedUrl, JenkinsView.class);
+        String url = prepareUrl(configuration.getBaseUrl());
+
+        System.out.println(url);
+
+        ResponseEntity<JenkinsView> response = restTemplate.exchange(url, HttpMethod.GET, request, JenkinsView.class);
+        JenkinsView jenkinsView = response.getBody();
+
         return jenkinsView.getJobs();
+    }
+
+    //TODO: refactore to http request builder class
+    private HttpEntity<String> getStringHttpEntity() {
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json"  );
+
+        return new HttpEntity<>(headers);
+    }
+
+    public List<JenkinsBuildPipeline> getBuildsFor(JenkinsJob jenkinsJob){
+        return Lists.newArrayList();
     }
 
     public JenkinsBuild getLastBuild(JenkinsJob jenkinsJob){
         RestTemplate restTemplate = new RestTemplate();
 
-        String url = prepareUrl(jenkinsJob.getUrl());
+        HttpEntity<String> request = getStringHttpEntity();
 
-        JenkinsBuildPipeline pipeline = restTemplate.getForObject(url, JenkinsBuildPipeline.class);
+        String url = prepareUrl(jenkinsJob.getUrl());
+        System.out.println(url);
+        ResponseEntity<JenkinsBuildPipeline> response = restTemplate.exchange(url, HttpMethod.GET, request, JenkinsBuildPipeline.class);
+        JenkinsBuildPipeline pipeline = response.getBody();
 
         JenkinsBuild jenkinsBuild = new JenkinsBuild();
 
@@ -53,12 +81,27 @@ public class DataProvider {
     }
 
     public String getChangingUserFor(String lastBuildUrl) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<String> request = getStringHttpEntity();
+
+        String url = prepareUrl(lastBuildUrl);
+
+        System.out.println(url);
+
+//        ResponseEntity<JenkinsBuildPipeline> response = restTemplate.exchange(url, HttpMethod.GET, request, JenkinsBuildPipeline.class);
+//        JenkinsBuildPipeline jenkinsBuildPipeline = response.getBody();
+
         return "bich152";
     }
 
+    //TODO: refactore to url handling class
     private String prepareUrl(String jenkinsJobUrl) {
 
-        return jenkinsJobUrl+= API_JSON_URL_ENDING;
+        String preparedUrl = StringUtils.replace(jenkinsJobUrl, configuration.getUrlSearchPattern(),
+                configuration.getUrlReplacement());
+        preparedUrl += API_JSON_URL_ENDING;
 
+        return preparedUrl;
     }
 }
